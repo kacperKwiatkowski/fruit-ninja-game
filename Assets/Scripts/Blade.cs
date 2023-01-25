d85d23b26665d91bfbc0c71dc308f26bd09e3691
kacperKwiatkowski <kacper18k@gmail.com>
2023-01-25 13:45:48 +0100

    Project switched to android.

--- Assets/Scripts/Blade.cs
+++ Assets/Scripts/Blade.cs
@@ -5,16 +5,28 @@ using UnityEngine;
 
 public class Blade : MonoBehaviour
 {
+
+    public float minVelo = 0.1f;
+    private Vector3 lastMousePos;
+    private Vector3 mouseVelo;
+
+    private Collider2D col;
     private Rigidbody2D rb;
     
     void Awake()
     {
         rb = GetComponent<Rigidbody2D>();
+        col = GetComponent<Collider2D>();
     }
 
     private void Update()
     {
         SetBladeToMouse();
+    }
+    
+    private void FixedUpdate()
+    {
+        col.enabled = IsMouseMoving();
     }
 
     private void SetBladeToMouse()
@@ -22,5 +34,18 @@ public class Blade : MonoBehaviour
         var mousePosition = Input.mousePosition;
         mousePosition.z = 10;
         rb.position = Camera.main.ScreenToWorldPoint(mousePosition);
+    }
+
+    private bool IsMouseMoving()
+    {
+        Vector3 curMousePos = transform.position;
+        float traveled = (lastMousePos - curMousePos).magnitude;
+        lastMousePos = curMousePos;
+
+        if (traveled > minVelo)
+        {
+            return true;
+        }
+        else return false;
     }
 }
