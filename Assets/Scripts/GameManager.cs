1fed50b5b5acd018dcf3384d6b3417588de43d4e
kacperKwiatkowski <kacper18k@gmail.com>
2023-01-25 11:56:36 +0100

    Highscore implemented

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -10,37 +10,53 @@ public class GameManager : MonoBehaviour
 
     [Header("Score Elements")]
     public int score;
+    public int highScore;
     public TextMeshProUGUI scoreText;
+    public TextMeshProUGUI highScoreText;
     
     [Header("GameOver")]
     public GameObject gameOverPanel;
     public TextMeshProUGUI gameOverPanelScoreText;
+    public TextMeshProUGUI gameOverPanelHighScoreText;
     
     private void Awake()
     {
         gameOverPanel.SetActive(false);
+        GetHighScore();
+    }
+
+    private void GetHighScore()
+    {
+        highScore = PlayerPrefs.GetInt("HighScore");
+        highScoreText.text = "Best: " + highScore;
     }
 
     public void IncreaseScore(int point)
     {
         score += point;
         scoreText.text = score.ToString();
+
+        if (highScore < score)
+        {
+            PlayerPrefs.SetInt("HighScore", score);
+            highScoreText.text =  "Best: " + score.ToString();
+        }
     }
 
     public void OnBombHit()
     {
         Time.timeScale = 0;
         gameOverPanelScoreText.text = "Score: " + score.ToString();
+        gameOverPanelHighScoreText.text = "Best: " + highScore.ToString();
         gameOverPanel.SetActive(true);
     }
 
     public void RestartGame()
     {
         score = 0;
-        scoreText.text = "0";
+        scoreText.text = score.ToString();
         
         gameOverPanel.SetActive(false);
-        gameOverPanelScoreText.text = "Score: 0";
 
         foreach (GameObject g in GameObject.FindGameObjectsWithTag("Interactable"))
         {
